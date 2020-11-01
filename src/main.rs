c355228f5dd72b0e09b9cb4f03aa90b7ec734cab
EnderDev <kieran@dothq.co>
2020-11-01 16:27:14 +0000

    Add entry widget

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+extern crate gtk;
+extern crate gio;
+
+use gtk::prelude::*;
+use gio::prelude::*;
+
+use gtk::{Application, Entry};
+
+fn build_ui(application: &gtk::Application) {
+    let window = gtk::ApplicationWindowBuilder::new()
+                                        .application(application)
+                                        .title("Calculator")
+                                        .border_width(20)
+                                        .window_position(gtk::WindowPosition::Center)
+                                        .default_width(400)
+                                        .default_height(550)
+                                        .build();
+
+    let entry = Entry::new();
+
+    window.add(&entry);
+
+    window.show_all();
+}
+
 fn main() {
-    println!("Hello, world!");
+    let application = Application::new(
+        Some("co.dothq.gtk-demo"),
+        Default::default()
+    ).expect("failed to initialize GTK application");
+
+    application.connect_activate(move |app| {
+        build_ui(app);
+    });
+
+    application.run(&[]);
 }
